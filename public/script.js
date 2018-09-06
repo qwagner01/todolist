2cd24b4a4eb24603bfad67053f047102cafc6eeb
qwagner01 <qwagner01@gmail.com>
2018-09-06 08:17:50 -0400

    added jquery

--- public/script.js
+++ public/script.js
@@ -1,11 +1,10 @@
 var list = []
-window.onload = function(){
-  var button = document.getElementById('button');
+$(document).ready(function(){  var button = document.getElementById('button');
   var input = document.getElementById('input');
   var read = document.getElementById('read');
   read.addEventListener('click', sendList);
   button.addEventListener('click',add)
-}
+});
 
 function add(){
   var li = document.createElement("li");
@@ -30,5 +29,5 @@ function sendList(){
   xhttp.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
   xhttp.send(arrStr);
   list = [];
-
+  $('li').remove();
 }
